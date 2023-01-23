f54c668512bad38a17447c406617ff15732f18c4
Poipurin <b.pinorojo@gmail.com>
2023-01-23 12:42:46 -0300

    bucle while

---
+++ Javascript/js/bucle_while.js
@@ -0,0 +1,31 @@
+//WHILE
+/*
+while(condicion){
+
+}
+
+*/
+var contador = 0;//definiendo variable
+while (contador <= 5) {//validando, mientras sea verdadero entra al while 
+    console.log("Somos de pana los turing!!");
+    contador++;//sumar uno a la variable contador
+}
+
+
+//contador =2,
+
+var termino = true; //variable booleana (true o false)
+
+var contador = 10;
+console.log("contador", contador);
+//termino cuando contador sea ==+ 200
+while (termino) {
+
+    contador = contador + 10;
+
+    if (contador == 200) {
+        termino = false;
+
+    }
+
+}
