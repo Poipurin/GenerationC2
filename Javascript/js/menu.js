3979d01ac029ddc12483e475e518910b4a6023de
Poipurin <b.pinorojo@gmail.com>
2023-01-23 12:29:35 -0300

    mejoras

---
+++ Javascript/js/menu.js
@@ -0,0 +1,11 @@
+var nombre = "Bárbara";
+var apellido = "Pino";
+var edad = 29
+var direccion = "Mi casa"
+var numerodir = 123
+
+console.log("● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ●");
+console.log("●", "Nombre: ", nombre, apellido, "                                ●");
+console.log("●", "Edad: ", edad, "                                            ●")
+console.log("●", "Dirección: ", direccion, numerodir, "  ●")
+console.log("● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ● ●");
